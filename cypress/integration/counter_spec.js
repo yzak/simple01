7b678418fa7585fed76bb48c4ce10de211301cdd
yzak <yasuo1979@gmail.com>
2021-07-27 00:07:30 +0900

    add cypress sample

---
+++ cypress/integration/counter_spec.js
@@ -0,0 +1,16 @@
+describe('カウンターアプリのテスト', function () {
+    beforeEach(() => {
+      cy.visit('/');
+    });
+    it('+ボタンを押すとカウンターの値に1プラスされる', () => {
+      cy.get('.increment').click().get('.counter').should('have.text', '1')
+    });
+  
+    it('-ボタンを押すとカウンターの値が1マイナスされる', () => {
+      cy.get('.decrement').click().get('.counter').should('have.text', '-1')
+    });
+  
+    it('+ボタンを押した後、リセットボタンを押すとカウントが0に戻る', () => {
+      cy.get('.increment').click().get('.reset').click().get('.counter').should('have.text', '0')
+    });
+  });
--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/*
 import logo from './logo.svg';
 import './App.css';
 
@@ -21,5 +22,38 @@ function App() {
     </div>
   );
 }
+*/
+import React, {useState} from 'react';
+
+const App = () => {
+ const [count, setCount] = useState(0);
+
+ const increment = () => {
+  setCount(count + 1);
+ };
+
+ const decrement = () => {
+  setCount(count - 1);
+ };
+
+ const handleReset = () => {
+  setCount(0);
+ };
+ return (
+  <div className="container">
+   <span className="counter">{count}</span>
+   <button className="increment" onClick={increment}>
+    +
+   </button>
+   <button className="decrement" onClick={decrement}>
+    -
+   </button>
+
+   <button className="reset" onClick={handleReset}>
+    数値をリセット
+   </button>
+  </div>
+ );
+};
 
 export default App;
